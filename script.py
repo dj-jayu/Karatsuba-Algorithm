c7724539747c7155165571d286b18e00b214d526
Douglas <73705447+dj-jayu@users.noreply.github.com>
2023-07-20 19:19:31 -0300

    Update script.py
    corrected the return expression so it works with large values

--- script.py
+++ script.py
@@ -1,3 +1,4 @@
+# count the number of digits in a number
 
 # count the number of digits in a number
 def count_dig(n):
@@ -12,7 +13,7 @@ def count_dig(n):
 # multiply two n-bit numbers using Karatsuba Algorithm
 def multiply(n1, n2):
     base = 10
-    n_dig = count_dig(n1)
+    n_dig = max(count_dig(n1), count_dig(n2))
 
     if n_dig == 1:
         return n1 * n2
@@ -26,8 +27,12 @@ def multiply(n1, n2):
 
     a = multiply(n1_left, n2_left)
     d = multiply(n1_right, n2_right)
-    e = (n1_left + n1_right) * (n2_left + n2_right) - a - d
-    return a*base**n_dig + e*base**(n_dig//2)+d
+    e = multiply(n1_left + n1_right, n2_left + n2_right) - a - d
+    return a * divisor ** 2 + e * divisor + d
 
-print(multiply(1234, 4321), 1234 * 4321)
+n1=3789814159265358979323846987264338327959870980902884197169399375105820974944592
+n2 = 909827188182845904528768760935360287471352662497757247093699959574966967627
 
+k=multiply(n1,n2)
+r=n1*n2
+print(r==k)
